58e67ced1f8b144d0fd0535a5998c82fa45d1d0e
Audrey-hub-dev <81632442+Audrey-hub-dev@users.noreply.github.com>
2022-03-01 19:33:46 +0100

    utilisation, importation, connexion à mongoose (base de données)

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const app = express();
 
+//importation mongoose 
+const mongoose = require('mongoose');
 
-app.use((req, res) => {
+// connexion à mongoDB
+mongoose.connect('mongodb+srv://audreyv:paris@cluster0.flvp6.mongodb.net/Cluster0?retryWrites=true&w=majority' , 
+
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+/*app.use((req, res) => {
     res.json ({message: 'votre requête a bien été reçue !' });
 })
+*/
+
+//gestion des CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+
+
+//pour analyser le corps de la requête 
+app.use(express.json());
 
 module.exports = app;
