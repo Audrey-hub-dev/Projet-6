bf9dc27b4fcb1a6e824454ea35257d4f1cbaaa11
Audrey-hub-dev <81632442+Audrey-hub-dev@users.noreply.github.com>
2022-03-19 15:47:37 +0100

    ajout Data ModelsSauce dans le shéma mongoose

--- backend/models/Sauce.js
+++ backend/models/Sauce.js
@@ -2,15 +2,31 @@ const mongoose = require('mongoose');
 
 //création d'un schéma de données qui contient les chmaps souhaités pour chaque sauce
 const sauceSchema = mongoose.Schema({
-
-
-
-  
-  title: { type: String, required: true },
+  //id du user : identifiant MongoDB unique de l'utilisateur qui a créé la sauce
+  userId : { type: String, required: true },
+  //nom de la sauce
+  name: { type: String, required: true },
+  //fabricant de la sauce 
+  manufacturer : { type: String, required: true },
+  //description de la sauce 
   description: { type: String, required: true },
-  imageUrl: { type: String, required: true },
-  userId: { type: String, required: true },
-  price: { type: Number, required: true },
-});
+  //le principal ingrédient épicé de la sauce 
+  mainPepper : { type: String, required: true },
+  //l'url de l'image de la sauce téléchargée par l'utilisateur 
+  imageUrl : { type: String, required: true },
+  //nombre entre 1 et 10 décrivant la sauce 
+  heat : { type: Number, required: true },
+  //nombre d'utilisateurs qui aiment la sauce 
+  likes : { type: Number },
+  //nombre d'utilisateurs qui n'aiment pas la sauce
+  dislikes : { type: Number },
+  //tableau des identifiants des utilisateurs qui ont aimé la sauce 
+  usersLiked : { type: ["String <userId>"] },
+  //tableau des identifiants des utilisateurs qui n'ont pas aimé la sauce 
+  usersDisliked : { type: ["String <userId>"] },
+  
+  });
 
-module.exports = mongoose.model('Thing', sauceSchema);
+
+//on exporte le schéma de données
+module.exports = mongoose.model('Sauce', sauceSchema);
