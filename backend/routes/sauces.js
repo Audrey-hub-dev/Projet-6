e093c947c92a8f3e93c37f1dccd1b6d427c70b1a
Audrey-hub-dev <81632442+Audrey-hub-dev@users.noreply.github.com>
2022-03-20 14:29:23 +0100

    importation auth, multer

--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -4,17 +4,23 @@ const router = express.Router();
 
 const saucesCtrl = require('../controllers/sauces'); 
 
+//importation du middleware d'authentification
+const auth = require('../middleware/auth'); 
 
+//importation du middleware multer
+const multer = require('../middleware/multer-config');
+
+//application de l'authentification à toutes les routes 
 router.get /*possibilité de mettre use aussi à la place 
-de get*/('/', /*auth*/ saucesCtrl.getAllSauces);
+de get*/('/', auth, saucesCtrl.getAllSauces);
 //création
-router.post('/' /*,auth, multer*/, saucesCtrl.createSauce);
+router.post('/' , auth, multer, saucesCtrl.createSauce);
 //obtenir
-router.get('/:id' /*,auth*/,  saucesCtrl.getOneSauce); 
+router.get('/:id' ,auth,  saucesCtrl.getOneSauce); 
 //mise à jour
-router.put('/:id'/*, auth, multer*/, saucesCtrl.modifySauce); 
+router.put('/:id', auth, multer, saucesCtrl.modifySauce); 
 //suppression 
-router.delete('/:id'/*,auth*/,  saucesCtrl.deleteSauce); 
+router.delete('/:id', auth,  saucesCtrl.deleteSauce); 
 
 
 module.exports = router;
