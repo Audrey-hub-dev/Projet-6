e03393b494158cb8b87f5ffbaf2072413c92000f
Audrey-hub-dev <81632442+Audrey-hub-dev@users.noreply.github.com>
2022-04-05 21:45:03 +0200

    reformulation fonction

--- backend/middleware/password-verification.js
+++ backend/middleware/password-verification.js
@@ -4,7 +4,12 @@ const passwordSchema = require('../models/password');
 //exportation de la fonction password 
 module.exports = (req, res, next) => {
     if (!passwordSchema.validate(req.body.password)) {
-        res.writeHead(400, "Le mot de passe doit contenir au minimum 8 caractères et contenir au moins une majuscule et une minuscule.", { 'content-type': 'application/json' });
+        let responseMessage = "Le mot de passe doit contenir";
+            responseMessage += " au minimum 8 caract\u00e8res et contenir";
+            responseMessage += " au moins une majuscule et une minuscule.";
+
+        res.writeHead(400, 
+            responseMessage, { 'content-type': 'application/json' });
         res.end('Format de mot de passe incorrect');
     } else {
         next();
