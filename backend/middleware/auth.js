ab4651e201443de1a8efd46959017caddc93f689
Audrey-hub-dev <81632442+Audrey-hub-dev@users.noreply.github.com>
2022-04-05 16:43:12 +0200

    ajout d'une authentification pour les fonctions modify et delete

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,14 +3,16 @@
  * sécurité. 
  */
 
+
 //utilisation du package jwt pour vérifier les token
 const jwt = require('jsonwebtoken');
+
 
 //exportation du middleware d'authentification 
 module.exports = (req, res, next) => {
   try {
-      /*récupération du token dans le header, dans network, headers, 
-      authorization, la chaine de caractères est le token*/
+    //récupération du token dans le header, dans network, headers, 
+    //authorization, la chaine de caractères est le token
     const token = req.headers.authorization.split(' ')[1/*deuxième 
     élément de ce tableau qui est a chaine de caractères*/];
     //décoder le token
@@ -18,15 +20,21 @@ module.exports = (req, res, next) => {
     doit correspondre à la clé dans la fonction login*/);
     //récupération de l'user id qui est dans le token 
     const userId = decodedToken.userId;
-    if (req.body.userId && req.body.userId !== userId) {
+            
+    //on ajoute un objet auth à l'objet de requête qui contient le userId extrait du token 
+    //pour que seul celui à qui appartient la sauce puisse modifier ou supprimer celle-ci
+    req.auth = {userId}; 
+    //s’il y a une différence entre l’userId dans la requête du front-end et l’userId dans le token
+    if (req.body.userId && (req.body.userId !== userId)) {
         //ne pas authentifier la requête
       throw 'Invalid user ID';
     } else {// si tout va bien on peut passer la requête au prochain middleware
+      //créer, modifier, supprimer une sauce
       next();
     }
-  } catch {
-    res.status(401).json({
-      error: new Error('Invalid request!')
-    });
+  } 
+  catch (error) {
+    res.status(401).json({error}); 
   }
 };
+
