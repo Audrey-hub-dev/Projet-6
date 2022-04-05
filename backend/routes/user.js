bebba0acec483d34575106a212bd98276bf47240
Audrey-hub-dev <81632442+Audrey-hub-dev@users.noreply.github.com>
2022-04-05 18:07:59 +0200

    ajout dans route signup le mot de passe

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -11,8 +11,11 @@ const router = express.Router();
 //configuration du router pour associer les fonctions du contrôleur aux différentes routes
 const userCtrl = require('../controllers/user');
 
+//
+const passwordVerification = require('../middleware/password-verification');
+
 //création de deux routes post en précisant l'utilisation de la fonction signup et de la fonction login
-router.post('/signup', userCtrl.signup);
+router.post('/signup', passwordVerification, userCtrl.signup);
 router.post('/login', userCtrl.login);
 
 //exportation du routeur pour ensuite l'importer dans app.js
